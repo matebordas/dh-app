3f456818a319b53356ab8a93ea116f6c2411fcc0
Mate Balo Bordas <mate.balob@gmail.com>
2016-12-11 02:13:53 +0100

    carousel fixes

--- src/app/components/footer-section/footer-section.component.ts
+++ src/app/components/footer-section/footer-section.component.ts
@@ -1,5 +1,5 @@
-import {Component, ViewChild, ElementRef} from '@angular/core';
-import * as $ from 'jquery';
+import {Component} from "@angular/core";
+import * as $ from "jquery";
 
 require('./footer-section.component.scss');
 
@@ -11,18 +11,12 @@ require('./footer-section.component.scss');
         <header>
               <h3>Was wird über die <strong>LieferHeld App</strong> gesagt</h3>
               
-              <div class="carousel-wrapper">
-                  
-               </div>
-               
-                <div class="carousel-element">
-                      <div><h3>1</h3></div>
-                      <div><h3>2</h3></div>
-                      <div><h3>3</h3></div>
-                      <div><h3>4</h3></div>
-                      <div><h3>5</h3></div>
-                      <div><h3>6</h3></div>
-               </div>
+            <div class="carousel-element">
+                <span *ngFor="let item of quotes; let i = index;">
+                    <div class="name-text">{{item.user}}</div>
+                    <div class="quote-text">{{item.quote}}</div>
+                  </span>
+            </div>
         </header>
 
         <main class="main-content">
@@ -32,15 +26,45 @@ require('./footer-section.component.scss');
 })
 export class FooterSectionComponent {
 
+    private quotes = [
+        {
+            user: "Claire C",
+            quote: "Quick and easy, fantastic! I can order food on the go when I've left work!!"
+        },
+        {
+            user: "Alonso C",
+            quote: "Brilliant, quick and easy! Downloaded, chose my food and paid all in 5 minutes!"
+        },
+        {
+            user: "Magedelna S",
+            quote: "I love Liferheld, great idea. Fast and good quality service. Love the app too."
+        },
+        {
+            user: "Claire C",
+            quote: "Quick and easy, fantastic! I can order food on the go when I've left work!!"
+        },
+        {
+            user: "Alonso C",
+            quote: "Brilliant, quick and easy! Downloaded, chose my food and paid all in 5 minutes!"
+        },
+        {
+            user: "Magedelna S",
+            quote: "I love Liferheld, great idea. Fast and good quality service. Love the app too."
+        }
+    ];
+
     constructor() { }
 
-    ngOnInit() {
+    ngAfterViewInit() {
+
         $('.carousel-element').slick({
             dots: false,
             infinite: true,
             slidesToShow: 3,
             slidesToScroll: 1,
             arrows: true,
+            prevArrow: "<span class='glyphicon glyphicon-chevron-left left-arrow'></span>",
+            nextArrow: "<span class='glyphicon glyphicon-chevron-right right-arrow'></span>",
             responsive: [
                 {
                     breakpoint: 1024,
